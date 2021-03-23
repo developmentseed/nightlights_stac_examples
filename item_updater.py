ac7bcfea6126734f45aedcb25d23399ca5042332
Sean Harkins <sharkinsgis@gmail.com>
2021-03-23 17:26:21 -0600

    Group objects in S3 by orbital segment.

--- item_updater.py
+++ item_updater.py
@@ -5,6 +5,7 @@ from pystac.extensions.eo import Band
 from datetime import datetime
 import boto3
 import json
+from iteration_utilities import groupedby
 
 bucket_path = "https://globalnightlight.s3.amazonaws.com"
 
@@ -132,54 +133,87 @@ def add_links(item, root_url, segment):
 STAC_IO.read_text_method = http_read_method
 STAC_IO.write_text_method = s3_write_method
 
-
-item_url = "https://globalnightlight.s3.amazonaws.com/npp_202008/SVDNB_npp_d20200801_t0048355_e0054159_b45395_c20200801045415683718_noac_ops.rade9.co.json"
-parsed_url = urlparse(item_url)
-root_key = parsed_url.path.split("/")[1]
-root_url = urlunparse((
-    parsed_url.scheme,
-    parsed_url.netloc,
-    root_key,
-    None,
-    None,
-    None
-))
-
-existing_item = Item.from_file(item_url)
-id_components = existing_item.id.split("_")
-orbital_segment = f"{id_components[1]}_{id_components[2]}_" \
-    f"{id_components[3]}_{id_components[4]}_{id_components[5]}"
-creation_stamp = id_components[6]
-
-start_time = datetime.strptime(
-    f"{id_components[2][1:]}{id_components[3][1:]}",
-    "%Y%m%d%H%M%S%f"
+s3 = boto3.client('s3')
+paginator = s3.get_paginator('list_objects_v2')
+response_iterator = paginator.paginate(
+    Bucket="globalnightlight",
+    Prefix="npp_202008"
 )
-creation_time = datetime.strptime(
-    creation_stamp[1:],
-    "%Y%m%d%H%M%S%f"
-)
-new_item = Item(
-    id=orbital_segment,
-    bbox=existing_item.bbox,
-    geometry=existing_item.geometry,
-    datetime=start_time,
-    properties={},
-)
-end_time = datetime.strptime(
-    f"{id_components[2][1:]}{id_components[4][1:]}",
-    "%Y%m%d%H%M%S%f"
-)
-new_item.common_metadata.start_datetime = start_time
-new_item.common_metadata.end_datetime = end_time
-new_item.common_metadata.created = creation_time
-new_item.common_metadata.gsd = 750
-new_item.common_metadata.platform = "s-npp"
-new_item.common_metadata.instruments = ["viirs"]
 
-add_assets(new_item, root_url, orbital_segment, creation_stamp)
-add_links(new_item, root_url, orbital_segment)
-new_item.validate()
-with open(f"{orbital_segment}.json", 'w') as outfile:
-    json.dump(new_item.to_dict(), outfile)
+objects = []
+for response in response_iterator:
+    objects = objects + response["Contents"]
+
+
+def get_segment(obj):
+    key = obj["Key"]
+    components = key.split("/")[1].split("_")
+    if len(components) > 6:
+        segment = f"{components[1]}_{components[2]}_" \
+            f"{components[3]}_{components[4]}_{components[5]}"
+    else:
+        segment = None
+    return segment
+
+
+def get_key(obj):
+    key = obj["Key"]
+    return key.split("/")[1]
+
+
+dct = groupedby(objects, key=get_segment, keep=get_key)
+for key in dct.keys():
+    print(key)
+    if key is not None:
+        print(dct[key])
+
+# item_url = "https://globalnightlight.s3.amazonaws.com/npp_202008/SVDNB_npp_d20200801_t0048355_e0054159_b45395_c20200801045415683718_noac_ops.rade9.co.json"
+# parsed_url = urlparse(item_url)
+# root_key = parsed_url.path.split("/")[1]
+# root_url = urlunparse((
+    # parsed_url.scheme,
+    # parsed_url.netloc,
+    # root_key,
+    # None,
+    # None,
+    # None
+# ))
+
+# existing_item = Item.from_file(item_url)
+# id_components = existing_item.id.split("_")
+# orbital_segment = f"{id_components[1]}_{id_components[2]}_" \
+    # f"{id_components[3]}_{id_components[4]}_{id_components[5]}"
+# creation_stamp = id_components[6]
+
+# start_time = datetime.strptime(
+    # f"{id_components[2][1:]}{id_components[3][1:]}",
+    # "%Y%m%d%H%M%S%f"
+# )
+# creation_time = datetime.strptime(
+    # creation_stamp[1:],
+    # "%Y%m%d%H%M%S%f"
+# )
+# new_item = Item(
+    # id=orbital_segment,
+    # bbox=existing_item.bbox,
+    # geometry=existing_item.geometry,
+    # datetime=start_time,
+    # properties={},
+# )
+# end_time = datetime.strptime(
+    # f"{id_components[2][1:]}{id_components[4][1:]}",
+    # "%Y%m%d%H%M%S%f"
+# )
+# new_item.common_metadata.start_datetime = start_time
+# new_item.common_metadata.end_datetime = end_time
+# new_item.common_metadata.created = creation_time
+# new_item.common_metadata.gsd = 750
+# new_item.common_metadata.platform = "s-npp"
+# new_item.common_metadata.instruments = ["viirs"]
+
+# add_assets(new_item, root_url, orbital_segment, creation_stamp)
+# add_links(new_item, root_url, orbital_segment)
+# new_item.validate()
+# with open(f"{orbital_segment}.json", 'w') as outfile:
+    # json.dump(new_item.to_dict(), outfile)
 # print(json.dumps(new_item.to_dict()))
